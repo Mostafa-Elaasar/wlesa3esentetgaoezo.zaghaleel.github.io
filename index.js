2cb3f9f3d0d6201f6811f36b8056dc5e0688946b
Mostafa-Elaasar <mostafa.elaasar.mitapp@gmail.com>
2025-12-27 15:22:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function epone(){window.location.href="ep1.html";}
+function eptwo(){window.location.href="ep2.html";}
+function epthree(){window.location.href="ep3.html";}
+function epfour(){window.location.href="ep4.html";}
+function epfive(){window.location.href="ep5.html";}
+function epsix(){window.location.href="ep6.html";}
+function epseven(){window.location.href="ep7.html";}
+function epeight(){window.location.href="ep8.html";}
